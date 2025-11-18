8d12276131c1bd49a2ef14cf252125800e557f69
Siqi Chen <chensiqi911@gmail.com>
2025-11-18 18:55:12 -0500

    Add Rating tests (happy path + exception test)

---
+++ tests/gold-five.Domain.Tests/RatingTests.cs
@@ -0,0 +1,18 @@
+﻿using Gold_Five.Domain.Catalog;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace gold_five.Domain.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void Can_Create_New_Rating()
+    {
+        var rating = new Rating(1, "Make", "Great fit!");
+
+        Assert.AreEqual(1, rating.Stars);
+        Assert.AreEqual("Make", rating.UserName);
+        Assert.AreEqual("Great fit!", rating.Review);
+    }
+}
